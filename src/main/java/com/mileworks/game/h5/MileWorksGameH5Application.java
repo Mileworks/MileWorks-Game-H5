0bd9b43659a3b8228cc19f97082fbb63ded7cd68
borrip <borrip0419@gmail.com>
2018-08-13 19:05:07 +0800

    init

---
+++ src/main/java/com/mileworks/game/h5/MileWorksGameH5Application.java
@@ -0,0 +1,12 @@
+package com.mileworks.game.h5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileWorksGameH5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MileWorksGameH5Application.class, args);
+	}
+}
